cde34873a83c51cdf9d751a5d9792737c526ba07
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 14:25:51 -0400

    Add prova-ue container IP to UE.sh

--- UE.sh
+++ UE.sh
@@ -12,7 +12,7 @@ fi
 sudo ip route add 10.241.115.0/24 via 240.84.80.231
 
 # PIETRO
-sudo ip route add 10.244.105.0/24 via 240.84.80.231
+sudo ip route add 10.244.105.0/24 via 240.84.80.251
 
 lxc restart $1
 lxc exec $1 bash
