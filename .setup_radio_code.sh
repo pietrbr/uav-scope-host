2d43fa8ec436d9405ec24c6ad8feb541eff8f526
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 14:00:25 -0400

    Remove unused script -setup_radio_code.sh

--- .setup_radio_code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script not in use
-echo 'script not in use'
-exit 0
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-#echo "$SCRIPT_DIR" # script directory
-
-cp -r $SCRIPT_DIR/radio_code/ ~/
